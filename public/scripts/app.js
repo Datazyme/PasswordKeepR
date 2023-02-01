df2806911095dc6467f547b5b53bb1034da0c93e
Archit Purohit'git config --global user.name Archit <Architpurohit@hotmail.com>
2023-01-31 20:11:46 -0800

    Dyanmic loading added

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,49 @@
 // Client facing scripts here
+document.getElementById("New_Account").addEventListener("click", function(){
+  var midSection = document.createElement("div");
+  midSection.setAttribute("id", "mid_section");
+
+  var div1 = document.createElement("span");
+  div1.setAttribute("id", "div1");
+  var h3 = document.createElement("h3");
+  h3.innerHTML = "website name placeholder";
+  div1.appendChild(h3);
+
+  var div2 = document.createElement("span");
+  div2.setAttribute("id", "div2");
+  var h4 = document.createElement("h4");
+  h4.innerHTML = "Username";
+  div2.appendChild(h4);
+  var input1 = document.createElement("input");
+  input1.setAttribute("type", "text");
+  input1.setAttribute("placeholder", "Textbox 2");
+  div2.appendChild(input1);
+
+  var div3 = document.createElement("span");
+  div3.setAttribute("id", "div3");
+  var h4 = document.createElement("h4");
+  h4.innerHTML = "password";
+  div3.appendChild(h4);
+  var input2 = document.createElement("input");
+  input2.setAttribute("type", "text");
+  input2.setAttribute("placeholder", "Textbox 1");
+  div3.appendChild(input2);
+
+  var div4 = document.createElement("span");
+  div4.setAttribute("id", "div4");
+  var addBtn = document.createElement("button");
+  addBtn.setAttribute("id", "add");
+  addBtn.innerHTML = "Add";
+  div4.appendChild(addBtn);
+  var editBtn = document.createElement("button");
+  editBtn.setAttribute("id", "edit");
+  editBtn.innerHTML = "Edit";
+  div4.appendChild(editBtn);
+
+  midSection.appendChild(div1);
+  midSection.appendChild(div2);
+  midSection.appendChild(div3);
+  midSection.appendChild(div4);
+
+  document.getElementById("New").appendChild(midSection);
+});
