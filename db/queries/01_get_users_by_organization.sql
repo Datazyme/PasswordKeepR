bde962ce8ff18e4a49555c694e25caa621dc0837
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-03 00:54:07 -0800

    write first db query

---
+++ db/queries/01_get_users_by_organization.sql
@@ -0,0 +1,5 @@
+SELECT organizations.name as organization, roles.name as role, users.name as name, email, master_password, master_password_hint, is_active
+FROM users
+JOIN organizations ON organizations.id = organization_id
+JOIN roles ON roles.id = role_id
+ORDER BY organization;
