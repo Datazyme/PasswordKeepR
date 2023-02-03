72032763e9f0b1fd5855eeb7f613198829ed78ca
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-03 01:46:15 -0800

    add is_active column

--- db/queries/02_get_credentials_by_users.sql
+++ db/queries/02_get_credentials_by_users.sql
@@ -1,4 +1,4 @@
-SELECT organizations.name as organization, roles.name as role_name, users.name as name, users.email as email, website, username, password, hint, category, require_master_password
+SELECT users.is_active as is_active, organizations.name as organization, roles.name as role_name, users.name as name, users.email as email, website, username, password, hint, category, require_master_password
 FROM credentials
 JOIN users ON users.id = credentials.user_id
 JOIN organizations ON organizations.id = credentials.organization_id
