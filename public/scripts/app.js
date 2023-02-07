1343d240b6ae9450e0759baab4f5039942771914
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-06 17:23:50 -0800

    fixed further conflict and linted code

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,14 @@
 // Client facing scripts here
 // refactor it to jquery
-document.getElementById("New_Account").addEventListener("click", function(){
-  var formElements = [    {      title: 'Website',      placeholder: 'Website'    },    {      title: 'Username',      placeholder: 'Username'    },    {      title: 'Password',      placeholder: 'Password'    }  ];
+document.getElementById("New_Account").addEventListener("click", function() {
+  let formElements = [    {      title: 'Website',      placeholder: 'Website'    },    {      title: 'Username',      placeholder: 'Username'    },    {      title: 'Password',      placeholder: 'Password'    }  ];
 
-  var midSection = document.createElement("div");
+  let midSection = document.createElement("div");
   midSection.setAttribute("class", "mid_section");
 
-  var select = document.createElement("select");
+  let select = document.createElement("select");
   select.setAttribute("id", "pulldown");
-  var options = `
+  let options = `
     <option value="">Select a category</option>
     <option value="social media">Social Media</option>
     <option value="gaming">Gaming</option>
@@ -19,17 +19,17 @@ document.getElementById("New_Account").addEventListener("click", function(){
   midSection.appendChild(select);
 
   formElements.forEach(function(formElement, index) {
-    var div = document.createElement("span");
+    let div = document.createElement("span");
     div.setAttribute("class", `div${index + 1}`);
 
-    var h4 = document.createElement("h4");
+    let h4 = document.createElement("h4");
     h4.innerHTML = formElement.title;
     div.appendChild(h4);
 
-    var form = document.createElement("form");
+    let form = document.createElement("form");
     form.setAttribute("onsubmit", `submitForm(event, 'div${index + 1}')`);
 
-    var input = document.createElement("input");
+    let input = document.createElement("input");
     input.setAttribute("type", "text");
     input.setAttribute("id", `div${index + 1}-input`);
     input.setAttribute("placeholder", formElement.placeholder);
@@ -37,14 +37,14 @@ document.getElementById("New_Account").addEventListener("click", function(){
 
     div.appendChild(form);
 
-    var output = document.createElement("div");
+    let output = document.createElement("div");
     output.setAttribute("id", `div${index + 1}-output`);
     output.setAttribute("style", "display: none");
     div.appendChild(output);
     midSection.appendChild(div);
   });
 
-  var buttons = `
+  let buttons = `
     <span class="div4">
       <button class="add">Add</button>
       <button class="edit">Edit</button>
@@ -57,14 +57,14 @@ document.getElementById("New_Account").addEventListener("click", function(){
 
 
 // copy button listener
-var clipboard = new ClipboardJS('#copy-button');
+let clipboard = new ClipboardJS('#copy-button');
 
 clipboard.on('success', function(e) {
-    console.log(e);
+  console.log(e);
 });
 
 clipboard.on('error', function(e) {
-    console.log(e);
+  console.log(e);
 });
 
 // for Jerome to add to database
@@ -83,7 +83,7 @@ function submitForm(event, div) {
 }
 
 document.querySelector(".add").addEventListener("click", function() {
-  console.log("test")
+  console.log("test");
 });
 
 //target all the necessary HTML elements with unique identifiers
@@ -92,8 +92,8 @@ const passLength = document.querySelector("#length");
 const passLengthResult = document.querySelector("#length-result");
 const includeNumbers = document.querySelector("#numbers");
 const includeSymbols = document.querySelector("#symbols");
-const includeUppercase = document.querySelector("#upper-case")
-const includeLowercase = document.querySelector("#lower-case")
+const includeUppercase = document.querySelector("#upper-case");
+const includeLowercase = document.querySelector("#lower-case");
 const generateBtn = document.querySelector("#generate");
 // const copyPass = document.querySelector("#copy");
 
@@ -144,7 +144,7 @@ document.addEventListener("DOMContentLoaded", () => {
 // Listen for password range change
 passLength.addEventListener("change", (event) => {
   passLengthResult.innerText = event.target.value;
-  console.log(passLengthResult.innerText)
+  console.log(passLengthResult.innerText);
 });
 
 generateBtn.addEventListener("click", () => {
@@ -179,41 +179,43 @@ let randomPassword = function generatePassword(length, number, symbol, upper) {
   }
   const finalPassword = newPassword.slice(0, length);
   return finalPassword;
-}
+};
 
 function getRandomLower() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-};
+}
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-};
+}
 function getRandomSymbol() {
   const symbols = "!@#$%^&*(){}[]=<>/,.";
   return symbols[Math.floor(Math.random() * symbols.length)];
-};
+}
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97).toUpperCase();
-};
+}
 
-$(document).ready(function(){
-  // step 1 target
-  // step 2 add event listener
-  // step 3 effect
-    $(".test-password").submit(function(event){
-      event.preventDefault()
-      const input = $(".text-box").val()
-      $.ajax({
-        method: "POST",
-        url: "/api/passwords",
-        data: input
-      })
-    })
-  });// jerome's code
-$(document).ready(function(){
+// $(document).ready(function() {
+//   // step 1 target
+//   // step 2 add event listener
+//   // step 3 effect
+//   $(".test-password").submit(function(event) {
+//     event.preventDefault();
+//     const input = $(".text-box").val();
+//     $.ajax({
+//       method: "POST",
+//       url: "/api/passwords",
+//       data: input
+//     });
+//   });
+// });
+
+// jerome's code
+$(document).ready(function() {
 
   //helper function that returns the data in html format
   const getPasswords = (object) => {
-    const $password = `
+    const password = `
     <tr>
       <td class="password_id" style="display:none;">${object.id}</td>
       <td class="password_website">${object.website}</td>
@@ -225,7 +227,7 @@ $(document).ready(function(){
       <td><input type="submit" value="Edit" form="password-edit" id="edit-button"></td>
       <td><input type="submit" value="Delete" form="password-delete" id="delete-button"></td>
     </tr>
-    `);
+    `;
     return password;
   };
 
@@ -258,14 +260,14 @@ $(document).ready(function(){
     return edit_box;
   };
 
-const loadEntry = function() {
+  const loadEntry = function() {
     $.ajax('/api/passwords', { method: 'GET' })
       .then((response) => {
         console.log("your page is grabbing the tweets from database", response);
         //loops through JSON object from api/passwords route and append every entry to the password table
         for (const password of response.passwords) {
-        $('.passwords-container').prepend(getPasswords(password))
-      }
+          $('.passwords-container').prepend(getPasswords(password));
+        }
         //(password);
       })
       .catch((err) => {
@@ -285,21 +287,21 @@ const loadEntry = function() {
   //   });
 
 
-    // POST request:
-    $("#new-password-form").submit(function(event){
-      event.preventDefault()
-      const user_id = 1;
-      const organization_id = 1;
-      const role_id = 3;
-      const is_user_created = true;
-      const category = $(".new-category").val();
-      const website = $(".new-website").val();
-      const username = $(".new-username").val();
-      const password = $(".new-password").val();
-      const hint = $(".new-hint").val();
-      const require_master_password = $(".new-require-master").val();
-      // console.log('submit')
-      $.post('/api/passwords',
+  // POST request:
+  $("#new-password-form").submit(function(event) {
+    event.preventDefault();
+    const user_id = 1;
+    const organization_id = 1;
+    const role_id = 3;
+    const is_user_created = true;
+    const category = $(".new-category").val();
+    const website = $(".new-website").val();
+    const username = $(".new-username").val();
+    const password = $(".new-password").val();
+    const hint = $(".new-hint").val();
+    const require_master_password = $(".new-require-master").val();
+    // console.log('submit')
+    $.post('/api/passwords',
       {
         user_id,
         organization_id,
@@ -311,57 +313,59 @@ const loadEntry = function() {
         hint,
         category,
         require_master_password
-      })
-      console.log('submit');
-      loadEntry();
-    });
-
-
-        $("#password-delete").submit(function(event){
-          // event.preventDefault()
-          const password_id = $(event.originalEvent.submitter).parent().siblings('.password_id').text();
-          $.post('/api/passwords/delete', { password_id })
-        });
-
-        $("#password-edit").submit(function(event){
-          event.preventDefault()
-          // const childrenSelector = $(event.originalEvent.submitter).parent().parent();
-          var childrenSelector = (selector) => {
-            return $(event.originalEvent.submitter).parent().parent().children(`${selector}`).text();
-          }
-          const data = {
-            id: childrenSelector('.password_id'),
-            website: childrenSelector('.password_website'),
-            username: childrenSelector('.password_username'),
-            password: childrenSelector('.password_password'),
-            hint: childrenSelector('.password_hint'),
-            category: childrenSelector('.password_category'),
-            require_master: childrenSelector('.password_require_master')
-          }
-          // console.log($(event.originalEvent.submitter).parent().parent())
-          $(event.originalEvent.submitter).parent().parent().replaceWith(editPasswords(data))
-        });
-
-        $("#password-edit-submit").submit(function(event) {
-          // event.preventDefault()
-
-          var siblingsSelector = (selector) => {
-            return $(event.originalEvent.submitter).parent().siblings(`${selector}`).children().val();
-          }
-
-          const data = {
-            id: $(event.originalEvent.submitter).parent().siblings('.password_id').text(),
-            website: siblingsSelector('.edit-password_website'),
-            username: siblingsSelector('.edit-password_username'),
-            password: siblingsSelector('.edit-password_password'),
-            hint: siblingsSelector('.edit-password_hint'),
-            category: siblingsSelector('.edit-password_category'),
-            require_master_password: siblingsSelector('.edit-password_require_master')
-          }
-
-          $.post('/api/passwords/edit', data)
-        });
-
-
       });
+    // console.log('submit');
+    loadEntry();
+  });
+
+
+  $("#password-delete").submit(function(event) {
+    event.preventDefault()
+    const password_id = $(event.originalEvent.submitter).parent().siblings('.password_id').text();
+    $.post('/api/passwords/delete', { password_id });
+    loadEntry();
+  });
+
+  $("#password-edit").submit(function(event) {
+    event.preventDefault();
+    // const childrenSelector = $(event.originalEvent.submitter).parent().parent();
+    let childrenSelector = (selector) => {
+      return $(event.originalEvent.submitter).parent().parent().children(`${selector}`).text();
+    };
+    const data = {
+      id: childrenSelector('.password_id'),
+      website: childrenSelector('.password_website'),
+      username: childrenSelector('.password_username'),
+      password: childrenSelector('.password_password'),
+      hint: childrenSelector('.password_hint'),
+      category: childrenSelector('.password_category'),
+      require_master: childrenSelector('.password_require_master')
+    };
+    // console.log($(event.originalEvent.submitter).parent().parent())
+    $(event.originalEvent.submitter).parent().parent().replaceWith(editPasswords(data));
+  });
+
+  $("#password-edit-submit").submit(function(event) {
+    event.preventDefault()
+
+    let siblingsSelector = (selector) => {
+      return $(event.originalEvent.submitter).parent().siblings(`${selector}`).children().val();
+    };
+
+    const data = {
+      id: $(event.originalEvent.submitter).parent().siblings('.password_id').text(),
+      website: siblingsSelector('.edit-password_website'),
+      username: siblingsSelector('.edit-password_username'),
+      password: siblingsSelector('.edit-password_password'),
+      hint: siblingsSelector('.edit-password_hint'),
+      category: siblingsSelector('.edit-password_category'),
+      require_master_password: siblingsSelector('.edit-password_require_master')
+    };
+
+    $.post('/api/passwords/edit', data);
+    loadEntry();
+  });
+
+
+});
 
