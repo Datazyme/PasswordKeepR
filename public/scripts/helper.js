4eb6bc65ce4dbe8c8de5e1cf968f98f78af2b609
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-07 06:36:14 -0800

    rename function name to postNewPassword

--- public/scripts/helper.js
+++ public/scripts/helper.js
@@ -29,35 +29,34 @@ const loadAllPasswords = function() {
     });
 };
 
-const handleSubmit = (event) => {
+//callback function when new username and password is added
+const postNewPassword = (event) => {
   event.preventDefault();
-    const user_id = 1;
-    const organization_id = 1;
-    const role_id = 3;
-    const is_user_created = true;
-    const category = $(".new-category").val();
-    const website = $(".new-website").val();
-    const username = $(".new-username").val();
-    const password = $(".new-password").val();
-    const hint = $(".new-hint").val();
-    const require_master_password = $(".new-require-master").val();
-    // console.log('submit')
-    $.post('/api/passwords',
-      {
-        user_id,
-        organization_id,
-        role_id,
-        is_user_created,
-        website,
-        username,
-        password,
-        hint,
-        category,
-        require_master_password
-      })
-      .then(() =>{
-        loadEntry();
-
-      })
-    // console.log('submit');
+  const user_id = 1;
+  const organization_id = 1;
+  const role_id = 3;
+  const is_user_created = true;
+  const category = $(".new-category").val();
+  const website = $(".new-website").val();
+  const username = $(".new-username").val();
+  const password = $(".new-password").val();
+  const hint = $(".new-hint").val();
+  const require_master_password = $(".new-require-master").val();
+  // console.log('submit')
+  $.post('/api/passwords',
+  {
+    user_id,
+    organization_id,
+    role_id,
+    is_user_created,
+    website,
+    username,
+    password,
+    hint,
+    category,
+    require_master_password
+  })
+  .then(() =>{
+    loadNewPassword();
+  })
 }
