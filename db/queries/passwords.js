da0442dbc7fff52c5f341f5d8ed6b8c77650f61d
Jerome <101889450+jeromealmir@users.noreply.github.com>
2023-02-08 20:26:36 -0800

    add organizations_name in query

--- db/queries/passwords.js
+++ db/queries/passwords.js
@@ -1,7 +1,7 @@
 const db = require('../connection');
 
 const getPasswords = (object) => {
-  return db.query('SELECT * FROM credentials WHERE user_id = $1 ORDER BY id', [object.id])
+  return db.query('SELECT organizations.name as organizations_name, credentials.* FROM credentials JOIN organizations ON organizations.id = organization_id WHERE user_id = $1 ORDER BY credentials.id', [object.id])
     .then(data => {
       return data.rows;
     });
